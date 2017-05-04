898e503607f307caf71f6f180af8f113e31bf5b1
Daniel Micay <danielmicay@gmail.com>
2017-05-03 21:20:55 -0400

    add simpler page sanitization
    The upstream option requires non-trivial debug infrastructure, so use
    something more like the earlier PaX implementation.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- kernel/power/snapshot.c
+++ kernel/power/snapshot.c
@@ -1145,6 +1145,7 @@ void free_basic_memory_bitmaps(void)
 
 void clear_free_pages(void)
 {
+#if defined(CONFIG_PAGE_POISONING_ZERO) || defined(CONFIG_PAGE_SANITIZE)
 	struct memory_bitmap *bm = free_pages_map;
 	unsigned long pfn;
 
@@ -1163,6 +1164,9 @@ void clear_free_pages(void)
 		memory_bm_position_reset(bm);
 		pr_info("free pages cleared after restore\n");
 	}
+	memory_bm_position_reset(bm);
+	pr_info("free pages cleared after restore\n");
+#endif /* PAGE_POISONING_ZERO || PAGE_SANITIZE */
 }
 
 /**
