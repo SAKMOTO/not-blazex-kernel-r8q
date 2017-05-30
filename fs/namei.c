4e88c9dd9ec78f7d5f06a07d9d905ee48d23152f
Daniel Micay <danielmicay@gmail.com>
2017-05-30 10:47:23 -0400

    enable protected_{symlinks,hardlinks} by default

--- fs/namei.c
+++ fs/namei.c
@@ -991,6 +991,7 @@ int sysctl_protected_hardlinks __read_mostly = 1;
 int sysctl_protected_fifos __read_mostly = 2;
 int sysctl_protected_regular __read_mostly = 2;
 
+
 /**
  * may_follow_link - Check symlink following for unsafe situations
  * @nd: nameidata pathwalk data
