4b6a2ca4afc6439955c69f4ee9295cc26ca04da6
LibXZR <i@xzr.moe>
2022-03-01 20:17:32 +0800

    devfreq_boost: Add support for boosting CPU_L3_LAT devices
    Signed-off-by: LibXZR <i@xzr.moe>

--- drivers/devfreq/devfreq_qcom_fw.c
+++ drivers/devfreq/devfreq_qcom_fw.c
@@ -17,6 +17,7 @@
 #include <linux/list.h>
 #include <linux/devfreq.h>
 #include <linux/pm_opp.h>
+#include <linux/devfreq_boost.h>
 
 #include <linux/sec_debug.h>
 #include <linux/sec_smem.h>
@@ -347,6 +348,15 @@ static int devfreq_qcom_fw_driver_probe(struct platform_device *pdev)
 	}
 
 	dev_info(dev, "Devfreq QCOM Firmware dev registered.\n");
+
+	if (!strcmp(dev_name(dev), "18590000.qcom,devfreq-l3:qcom,cpu0-cpu-l3-lat"))
+		devfreq_register_boost_device(DEVFREQ_MSM_CPU0_CPU_L3_LAT, df);
+
+	if (!strcmp(dev_name(dev), "18590000.qcom,devfreq-l3:qcom,cpu4-cpu-l3-lat"))
+		devfreq_register_boost_device(DEVFREQ_MSM_CPU4_CPU_L3_LAT, df);
+
+	if (!strcmp(dev_name(dev), "18590000.qcom,devfreq-l3:qcom,cpu7-cpu-l3-lat"))
+		devfreq_register_boost_device(DEVFREQ_MSM_CPU7_CPU_L3_LAT, df);
 
 	return 0;
 }
