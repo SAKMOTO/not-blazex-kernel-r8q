e32bf5805bc0c02833d167c92bc7ad107e9ddc07
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-10 22:51:30 -0800

    af_unix: Block libperfmgr from writing to the logd socket
    The logcat spam from libperfmgr when the kernel doesn't have everything it
    wants destroys logcats and creates excessive logd overhead from the sheer
    number of logs it prints.
    
    Block libperfmgr from writing to the logd socket so that it cannot write to
    logcat at all. This way, all libperfmgr attempts to write to logcat are
    killed early on before they can incur overhead, which solves the excessive
    logd overhead problem.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/open.c
+++ fs/open.c
@@ -1096,7 +1096,7 @@ struct file *file_open_root(struct dentry *dentry, struct vfsmount *mnt,
 }
 EXPORT_SYMBOL(file_open_root);
 
-bool task_is_powerhal(struct task_struct *p);
+bool task_is_libperfmgr(struct task_struct *p);
 static bool libperfmgr_redirect(struct file **f, int dfd, struct filename *n,
 				struct open_flags *op, int flags)
 {
@@ -1119,7 +1119,7 @@ static bool libperfmgr_redirect(struct file **f, int dfd, struct filename *n,
 	if (likely(*f != ERR_PTR(-ENOENT) ||
 	    (flags & REQUIRED_FLAGS) != REQUIRED_FLAGS ||
 	    flags & ~ALLOWED_FLAGS ||
-	    !task_is_powerhal(current)))
+	    !task_is_libperfmgr(current)))
 		return false;
 #undef ALLOWED_FLAGS
 #undef REQUIRED_FLAGS
