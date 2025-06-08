7edda175774643f01c66284cf7ecb980c27e9dd4
Your Name <you@example.com>
2025-06-08 18:10:16 -0300

    Revert "misc: minor logging tweaks and doc updates"
    This reverts commit e8796ac31b81e553852d837fa1d250791dc19c32.

--- init/version.c
+++ init/version.c
@@ -44,7 +44,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION " [ skye-pa1n custom build :P]\n";;
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char *linux_banner_ptr = linux_banner;
 EXPORT_SYMBOL_GPL(linux_banner_ptr);
