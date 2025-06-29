de4ecaa4b22ccd40cb86c010d5a0d06e49767645
skye-pa1n <sonicgames772@gmail.com>
2025-06-29 10:12:57 -0300

    drivers: gpu: KGSL: Remove Samsung IRQ affinity override

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5582,15 +5582,10 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	 * which IRQ's affinity is set to.
 	 */
 #ifdef CONFIG_SMP
-#ifdef CONFIG_DISPLAY_SAMSUNG
-	device->pwrctrl.pm_qos_req_dma.type = PM_QOS_REQ_AFFINE_CORES;
-	device->pwrctrl.pm_qos_req_dma.cpus_affine = 0xf;
-#else
 	device->pwrctrl.pm_qos_req_dma.type = PM_QOS_REQ_AFFINE_IRQ;
 	device->pwrctrl.pm_qos_req_dma.irq = device->pwrctrl.interrupt_num;
 #endif
 
-#endif
 	pm_qos_add_request(&device->pwrctrl.pm_qos_req_dma,
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
