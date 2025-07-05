9c3ed421511140a0a3117c0cf2fbc966ecda9c4d
skye-pa1n <sonicgames772@gmail.com>
2025-07-05 17:31:04 -0300

    drivers: Fixup compilation

--- drivers/misc/kprofiles/main.c
+++ drivers/misc/kprofiles/main.c
@@ -27,27 +27,13 @@
 #include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-#include <linux/msm_drm_notify.h>
-#elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
-#include <drm/drm_notifier_mi.h>
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#ifdef CONFIG_AUTO_KPROFILES_FB
 #include <linux/fb.h>
 #endif
 #include "version.h"
 #include <linux/notifier.h>
 
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-#define KP_EVENT_BLANK MSM_DRM_EVENT_BLANK
-#define KP_BLANK_POWERDOWN MSM_DRM_BLANK_POWERDOWN
-#define KP_BLANK_UNBLANK MSM_DRM_BLANK_UNBLANK
-#define kp_events msm_drm_notifier
-#elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
-#define KP_EVENT_BLANK MI_DRM_EVENT_BLANK
-#define KP_BLANK_POWERDOWN MI_DRM_BLANK_POWERDOWN
-#define KP_BLANK_UNBLANK MI_DRM_BLANK_UNBLANK
-#define kp_events mi_drm_notifier
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#ifdef CONFIG_AUTO_KPROFILES_FB
 #define KP_EVENT_BLANK FB_EVENT_BLANK
 #define KP_BLANK_POWERDOWN FB_BLANK_POWERDOWN
 #define KP_BLANK_UNBLANK FB_BLANK_UNBLANK
@@ -305,11 +291,7 @@ static inline int kp_register_display_notifier(void)
 {
 	int ret = 0;
 
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-	ret = msm_drm_register_client(&kp_display_notifier_block);
-#elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
-	ret = mi_drm_register_client(&kp_display_notifier_block);
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#ifdef CONFIG_AUTO_KPROFILES_FB
 	ret = fb_register_client(&kp_display_notifier_block);
 #endif
 
@@ -318,11 +300,7 @@ static inline int kp_register_display_notifier(void)
 
 static inline void kp_unregister_display_notifier(void)
 {
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-	msm_drm_unregister_client(&kp_display_notifier_block);
-#elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
-	mi_drm_unregister_client(&kp_display_notifier_block);
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#ifdef CONFIG_AUTO_KPROFILES_FB
 	fb_unregister_client(&kp_display_notifier_block);
 #endif
 }
